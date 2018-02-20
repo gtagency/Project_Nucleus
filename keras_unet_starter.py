30982af817c8b76ca21cfcd5834d6357a31d4e53
Evan Bretl <evanbretl4@gmail.com>
2018-02-20 18:40:23 -0500

    edits gitignore and file paths

--- keras_unet_starter.py
+++ keras_unet_starter.py
@@ -29,8 +29,8 @@ import tensorflow as tf
 IMG_WIDTH = 128
 IMG_HEIGHT = 128
 IMG_CHANNELS = 3
-TRAIN_PATH = '../input/stage1_train/'
-TEST_PATH = '../input/stage1_test/'
+TRAIN_PATH = 'input/stage1_train/'
+TEST_PATH = 'input/stage1_test/'
 
 warnings.filterwarnings('ignore', category=UserWarning, module='skimage')
 seed = 42
